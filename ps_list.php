7786670ad24fbc69d38f5821eda3fcb0cfcf1e6a
24sonib <bhanusoni24@gmail.com>
2018-07-17 11:08:01 +0530

    project layout change

--- ps_list.php
+++ ps_list.php
@@ -1,7 +1,9 @@
 <?php
-echo "<b><center>process</center></b>";
 echo "<pre>";
+echo '<h4 style="color:green">';
+echo "<b><center>process</center></b>";
 sleep(8);
 echo shell_exec('sudo docker ps');
+echo '</h4>';
 echo "</pre>";
 ?>
--- ps_list_filter.php
+++ ps_list_filter.php
@@ -1,10 +1,12 @@
 <?php
-echo "<b><center>Container-ID</center></b>";
 echo "<pre>";
+echo '<h4 style="color: green">';
+echo "<b><center>Container-ID</center></b>";
 sleep(8);
 //while(true){
 echo shell_exec("sudo docker ps| awk '{print $1}'");
-sleep(5);
+//sleep(5);
 //}
+echo '</h4>';
 echo "</pre>";
 ?>
